05996ba10520abdd116315395206f9c8b4a18cf7
ep1337 <eparks1337@gmail.com>
2023-03-03 18:58:10 -0500

    git merge errors so removing files

--- style.js
+++
@@ -1,8 +0,0 @@
-function topNavFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-      x.classList.add('responsive');
-    } else {
-      x.classList.remove('responsive');
-    }
-}
